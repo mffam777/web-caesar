edab09058008d0790e972344ddc45b48ff9d4835
AM <mffam777@gmail.com>
2018-12-01 20:07:41 -0500

    Mofify main.py HTML head section

--- main.py
+++ main.py
@@ -18,6 +18,29 @@ app.config['DEBUG'] = True
 form = """
 <!doctype html>
 <html>
+    <head>
+        <style>
+        form {
+            background-color: #eee;
+            padding: 20px;
+            margin: 0 auto;
+            width: 540px;
+            font: 16px sans-serif;
+            border-radius: 10px;
+        }
+    
+        textarea {
+            margin: 10px 0;
+            width: 540px;
+            height: 120px;
+        }
+    
+        p.error {
+            color: red;
+        }
+    </style>
+    </head>
+    
     <body>
         <form method="post">
             <div>
